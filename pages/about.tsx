675636823d0d86885343b31ee9ef35e4657a8ccb
songyh119 <yh.song@boozaapp.com>
2022-02-28 17:00:21 +0900

    feat: css modules for nextjs

---
+++ pages/about.tsx
@@ -0,0 +1,8 @@
+import NavBar from "../components/NavBar/NavBar";
+
+export default function Potato() {
+  return <div>
+    <NavBar />
+    <h1>About us</h1>
+  </div>
+}
---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react"
+import NavBar from "../components/NavBar/NavBar"
+
+export default function Home() {
+  return (
+    <div>
+      <NavBar />
+      <h1>Hello</h1>
+    </div>
+  )
+}
