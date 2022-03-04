88afa04e86593ad18733208423040a742645e34f
songyh119 <yh.song@boozaapp.com>
2022-03-04 17:11:27 +0900

    feat: passing queries

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,41 @@
 import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
-import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 import Seo from '../components/Seo';
 
 interface Movies {
   id: number;
   original_title: string;
-  poster_path: string;
+  poster_path?: string;
 }
 
 export default function Home({
   results,
 }: InferGetServerSidePropsType<GetServerSideProps>) {
+  const router = useRouter();
+  const handleMovieClick = (id: number, original_title: string) => {
+    router.push(
+      {
+        pathname: `/movies/${id}`,
+        query: {
+          title: original_title,
+        },
+      },
+      `/movies/${id}`,
+    );
+  };
+
   return (
     <div>
       <div className="container">
         <Seo title="home" />
-        {!results && <h4>Loading...</h4>}
         {results?.map((movie: Movies) => (
-          <div key={movie.id}>
-            <div className="movie" key={movie.id}>
-              <img
-                src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-              />
-              <h4>{movie.original_title}</h4>
-            </div>
+          <div
+            className="movie"
+            key={movie.id}
+            onClick={() => handleMovieClick(movie.id, movie.original_title)}
+          >
+            <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+            <h4>{movie.original_title}</h4>
           </div>
         ))}
         <style jsx>{`
